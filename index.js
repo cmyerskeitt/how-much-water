ce1544f72bd530addd61112cd09a4b1bea83637d
github username <cmyerskeitt@gmail.com>
2022-05-17 22:09:13 -0400

    final code and README

---
+++ index.js
@@ -0,0 +1,4 @@
+function howMuchWater(water, load, clothes){
+    return clothes < load ? "Not enough clothes" : clothes > 2 * load ? "Too much clothes" : 
+    +(water * 1.1 ** (clothes - load)).toFixed(2)
+} 
